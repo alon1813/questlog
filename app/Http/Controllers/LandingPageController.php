c9ca92c78ecef1f2c368c6f48d2f1f5de2a10c5a
Alejandro <alejandroalonfer@gmail.com>
2025-11-21 14:55:22 +0100

    arreglo carrusel

--- app/Http/Controllers/LandingPageController.php
+++ app/Http/Controllers/LandingPageController.php
@@ -26,11 +26,91 @@ class LandingPageController extends Controller
         }
 
         $popularItems = [
-            (object)['id' => 1, 'title' => 'Baldur\'s Gate 3', 'type' => 'game', 'image_url' => 'https://cdn.cloudflare.steamstatic.com/steam/apps/1086940/header.jpg?t=1695246593', 'link' => '#'],
-            (object)['id' => 2, 'title' => 'Frieren: Beyond Journey\'s End', 'type' => 'anime', 'image_url' => 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT9XensSLQi9eS5vQZJOqdoR9hnTp9ed9va1Ii9SeovpjtXTY4PW24Lf_UcZjcJEgjV1n4ApuvqWTFnNiqYWufJjGSznhMJsX4i13KiLg&s=10', 'link' => '#'],
-            (object)['id' => 3, 'title' => 'Cyberpunk 2077', 'type' => 'game', 'image_url' => 'https://i.blogs.es/b109e9/cyberpunk2077-johnny-v-rgb_en/1366_2000.jpg', 'link' => '#'],
-            (object)['id' => 4, 'title' => 'Jujutsu Kaisen S2', 'type' => 'anime', 'image_url' => 'https://m.media-amazon.com/images/S/pv-target-images/574a947b4f0b8e1f6007bc9f80df638fb1d8df5b7e833f9223295a09b8b080e0.jpg', 'link' => '#'],
-            (object)['id' => 5, 'title' => 'Final Fantasy VII Rebirth', 'type' => 'game', 'image_url' => 'https://img.asmedia.epimg.net/resizer/v2/UDSICW5LJRDRTA6X4PG3FH3T64.jpg?auth=da022ed3eef539abbd48865720b51e8cebe3ef04ac33b7f86737f07a18b81a57&width=1200&height=1200&smart=true', 'link' => '#'],
+            (object)[
+                'id' => 1, 
+                'title' => 'Baldur\'s Gate 3', 
+                'type' => 'game', 
+                'image_url' => 'https://cdn.cloudflare.steamstatic.com/steam/apps/1086940/header.jpg?t=1695246593', 
+                'link' => '#'
+            ],
+            (object)[
+                'id' => 2, 
+                'title' => 'Frieren: Beyond Journey\'s End', 
+                'type' => 'anime', 
+                'image_url' => 'https://m.media-amazon.com/images/M/MV5BZTI4ZGMxN2UtODlkYS00MTBjLWE1YzctYzc3NDViMGI0ZmJmXkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg', 
+                'link' => '#'
+            ],
+            (object)[
+                'id' => 3, 
+                'title' => 'Cyberpunk 2077', 
+                'type' => 'game', 
+                'image_url' => 'https://i.blogs.es/b109e9/cyberpunk2077-johnny-v-rgb_en/1366_2000.jpg', 
+                'link' => '#'
+            ],
+            (object)[
+                'id' => 4, 
+                'title' => 'Jujutsu Kaisen', 
+                'type' => 'anime', 
+                'image_url' => 'https://cdn.myanimelist.net/images/anime/1171/109222.jpg', 
+                'link' => '#'
+            ],
+            (object)[
+                'id' => 5, 
+                'title' => 'Final Fantasy VII Rebirth', 
+                'type' => 'game', 
+                'image_url' => 'https://cdn11.bigcommerce.com/s-hfy8688lak/images/stencil/1280x1280/products/2593/13618/FFVII_RB_AG_US__97253.1702580872.jpg?c=1', 
+                'link' => '#'
+            ],
+            // --- NUEVAS ADICIONES ---
+            (object)[
+                'id' => 6, 
+                'title' => 'Elden Ring', 
+                'type' => 'game', 
+                'image_url' => 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQU_cspEzfgMdenwt0VS7QHBJCOdWaVkrXyvA&s', 
+                'link' => '#'
+            ],
+            (object)[
+                'id' => 7, 
+                'title' => 'Demon Slayer', 
+                'type' => 'anime', 
+                'image_url' => 'https://cdn.myanimelist.net/images/anime/1286/99889.jpg', 
+                'link' => '#'
+            ],
+            (object)[
+                'id' => 8, 
+                'title' => 'Zelda: Tears of the Kingdom', 
+                'type' => 'game', 
+                'image_url' => 'https://www.nintendo.com/eu/media/images/10_share_images/games_15/nintendo_switch_4/2x1_NSwitch_TloZTearsOfTheKingdom_Gamepage_image1600w.jpg', 
+                'link' => '#'
+            ],
+            (object)[
+                'id' => 9, 
+                'title' => 'One Piece', 
+                'type' => 'anime', 
+                'image_url' => 'https://cdn.myanimelist.net/images/anime/6/73245.jpg', 
+                'link' => '#'
+            ],
+            (object)[
+                'id' => 10, 
+                'title' => 'Hollow Knight', 
+                'type' => 'game', 
+                'image_url' => 'https://cdn.cloudflare.steamstatic.com/steam/apps/367520/header.jpg', 
+                'link' => '#'
+            ],
+            (object)[
+                'id' => 11, 
+                'title' => 'Attack on Titan', 
+                'type' => 'anime', 
+                'image_url' => 'https://cdn.myanimelist.net/images/anime/10/47347.jpg', 
+                'link' => '#'
+            ],
+            (object)[
+                'id' => 12, 
+                'title' => 'God of War Ragnarök', 
+                'type' => 'game', 
+                'image_url' => 'https://media.revistagq.com/photos/613b43731df3ece1388a2f67/4:3/w_920,h_690,c_limit/God-of-War-Ragnarok-Featured-image.jpeg', 
+                'link' => '#'
+            ],
         ];
 
         return view('landing', compact('user', 'recentActivities', 'popularItems'));
